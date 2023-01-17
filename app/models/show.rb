273792b572eb6bdc5c4210470701a4dd9c9e5b18
Veronique Cadet <cadetvero@gmail.com>
2023-01-17 15:17:30 -0500

    commit tree 6f1cbbbeeffe81c2c8e650aa1efae366377ae2b6

---
+++ app/models/show.rb
@@ -0,0 +1,9 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        Show.maximum(:rating)
+    end
+
+    
+end
+                     
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+puts "seeding"
+Show.create(name: "Wargames", rating: 3, network: "John Badham", day: "monday", season: "two")
+Show.create(name: "The Sting", rating: 7, network: "George Roy Hill", day: "monday", season: "two")
+Show.create(name: "Nebraska", rating: 5, network: "Alexander Payne", day: "tuesday", season: "two")
+Show.create(name: "Muppets Most Wanted", rating: 4, network: "James Bobin", day: "thursday", season: "two")
+Show.create(name: "Zero Dark Thirty", rating: 7, network: "Kathryn Bigelow", day: "friday", season: "two")
